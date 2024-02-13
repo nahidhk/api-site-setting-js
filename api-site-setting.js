fa62e65f2172f2c909ff054e1a25b40511658522
nahidhk <nahidappltd@gmail.com>
2024-02-13 13:23:42 +0600

    new update

--- api-site-setting.js
+++ api-site-setting.js
@@ -1,7 +1,7 @@
 console.log("The api js");
 document.write(`
 <div id="api-found">
- <svg style="padding:5px;" onclick="rundatapto()" xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-gear" viewBox="0 0 16 16">
+ <svg title="Website Setting" style="padding:5px;cursor: pointer;" onclick="rundatapto()" xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-gear" viewBox="0 0 16 16">
   <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492M5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0"/>
   <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115z"/>
 </svg>
@@ -9,21 +9,40 @@ document.write(`
 `);
 callinopendata();
 function callinopendata(){
-document.getElementById('api-found').style="color: #fff;background-color:#343754A1; height: 50px;width: 50px;border-radius: 6px; position: fixed; top: 30%;left: 0;z-index: 10000;opacity: 0.5;";
+document.getElementById('api-found').style="color: #fff;background-color:#343754A1; height: 50px;width: 50px;border-radius: 6px; position: fixed; top: 30%;left: 0;z-index: 10000;opacity: 0.5;cursor: pointer;";
 }
-
-
+///////// This is body function
 function rundatapto(){
-document.getElementById('api-found').style="color: #fff;background-color:#343754; height: 250px;width: 170px;border-radius: 6px; position: fixed; top: 10%;left: 0;z-index: 10000;"
-document.getElementById('api-found').innerHTML=`<svg onclick="hellocall()" style="padding:5px; xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+document.getElementById('api-found').style="color: #fff;background-color:#343754; height: 320px;width: 170px;border-radius: 6px; position: fixed; top: 10%;left: 0;z-index: 10000;"
+document.getElementById('api-found').innerHTML=`<svg onclick="hellocall()" style="padding:5px;cursor: pointer;" xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
   <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
 </svg><span>setting</span><hr>
-<p id='ipoio' onclick="opendark()"> Lite</p>
+<p >
+<p id="dataxfftool" ><span onclick="opendark()" style="cursor: pointer;cursor: pointer;color: #fff;padding-left: 60px;" > <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" fill="currentColor" class="bi bi-moon-stars-fill" viewBox="0 0 16 16">
+<path d="M6 .278a.77.77 0 0 1 .08.858 7.2 7.2 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277q.792-.001 1.533-.16a.79.79 0 0 1 .81.316.73.73 0 0 1-.031.893A8.35 8.35 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.75.75 0 0 1 6 .278"/>
+<path d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.73 1.73 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.73 1.73 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.73 1.73 0 0 0 1.097-1.097zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.16 1.16 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.16 1.16 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732z"/>
+</svg> <span></p>
+</p><hr>
+<fieldset>
+<legend>Font Size</legend>
+<select style="background-color: #ffffff00;border: none;font-size: 13pt;outline:none;color:#fff;cursor: pointer;">
+  <option>10pt</option>
+  <option>11pt</option>
+  <option>13pt</option>
+  <option>15pt</option>
+  <option>17pt</option>
+  <option>19pt</option>
+  <option>21pt</option>
+  <option>22pt</option>
+  <option>23pt</option>
+  <option>24pt</option>
+</select>
+</fieldset>
 `
 }
-
+///////// ==================/==/==/==/=/=/=///////////////
 function hellocall(){
-  document.getElementById('api-found').style="color: #fff;background-color:#343754A1; height: 50px;width: 50px;border-radius: 6px; position: fixed; top: 30%;left: 0;z-index: 10000;opacity: 0.5;";
+  document.getElementById('api-found').style="color: #fff;background-color:#343754A1; height: 50px;width: 50px;border-radius: 6px; position: fixed; top: 30%;left: 0;z-index: 10000;opacity: 0.5;cursor: pointer;";
   document.getElementById('api-found').innerHTML=`
 
 <div id="api-found">
@@ -41,6 +60,10 @@ document.getElementsByTagName("body")[0].style.backgroundColor = "#42445A";
 document.getElementsByTagName("body")[0].style.color = "#FFFFFF";
 localStorage.setItem('darkbody','#42445A');
 localStorage.setItem('barkfont','#FFFFFF');
+
+document.getElementById("dataxfftool").innerHTML=`<span onclick="backdark()" style="cursor: pointer;cursor: pointer;color: #fff; padding-left: 60px;" > <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" fill="currentColor" class="bi bi-sun-fill" viewBox="0 0 16 16">
+<path d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708"/>
+</svg> <span>`;
 }
 allcallapkrunc();
 function allcallapkrunc(){
@@ -48,10 +71,17 @@ function allcallapkrunc(){
   var fontdataff1 = localStorage.getItem('barkfont');
  document.getElementsByTagName("body")[0].style.backgroundColor = datarunio1;
  document.getElementsByTagName("body")[0].style.color = fontdataff1;
-
 }
 
-styledatarun1();
-function styledatarun1(){
-  document.getElementById(ipoio).style="margin:10px"
+function  backdark(){
+  document.getElementsByTagName("body")[0].style.backgroundColor = "#fff";
+  document.getElementsByTagName("body")[0].style.color = "#000";
+  localStorage.removeItem("darkbody");
+  localStorage.removeItem("barkfont");
+  document.getElementById("dataxfftool").innerHTML=`<span onclick="opendark()" style="cursor: pointer;cursor: pointer;color: #fff;padding-left: 60px;" > <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" fill="currentColor" class="bi bi-moon-stars-fill" viewBox="0 0 16 16">
+  <path d="M6 .278a.77.77 0 0 1 .08.858 7.2 7.2 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277q.792-.001 1.533-.16a.79.79 0 0 1 .81.316.73.73 0 0 1-.031.893A8.35 8.35 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.75.75 0 0 1 6 .278"/>
+  <path d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.73 1.73 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.73 1.73 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.73 1.73 0 0 0 1.097-1.097zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.16 1.16 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.16 1.16 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732z"/>
+  </svg> <span>`
 }
+
+
